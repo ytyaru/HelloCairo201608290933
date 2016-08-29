35eab3ad3d0d11cd6b9345aa7d7750ada4461088
ytyaru <ytyaru@outlook.jp>
2016-08-29 19:51:25 +0900

    initial commit

---
+++ HelloCairo201608290933/Program.cpp
@@ -0,0 +1,34 @@
+﻿#include <gtk-2.0/gtk/gtk.h>
+#include <cairo.h>
+
+gboolean expose_event_callback(GtkWidget *widget, GdkEventExpose *event, gpointer data)
+{
+	cairo_t *cr;
+	cr = gdk_cairo_create(widget->window);
+
+	cairo_move_to(cr, 30, 30);
+	cairo_set_font_size(cr, 24);
+	cairo_show_text(cr, "Hello Cairo !!");
+
+	cairo_destroy(cr);
+	return FALSE;
+}
+int main(int argc, char* argv[])
+{
+	gtk_set_locale();
+	gtk_init(&argc, &argv);
+
+	GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Hello Cairo !! (GTK+, WindowsXP, VC++2010Express)");
+	gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+
+	GtkWidget *drawing_area = gtk_drawing_area_new();
+	gtk_widget_set_size_request (drawing_area, 100, 100);
+	gtk_container_add(GTK_CONTAINER(window), drawing_area);
+	g_signal_connect(G_OBJECT(drawing_area), "expose_event", G_CALLBACK(expose_event_callback), NULL);
+	
+	gtk_widget_show_all(window);
+	gtk_main();
+	return 0;
+}
---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+﻿# このソフトウェアについて #
+
+HelloGtkPlus201608252332は私個人が学習目的で作成したソフトウェアです。
+
+Cairoを使って文字列を描画しました。
+
+# 開発環境 #
+
+* Windows XP SP3
+* Visual C++ 2010 Express
+    * C++/Win32
+
+## ライブラリ ##
+
+* [glade3-3.6.7-with-GTK+.exe](http://ftp.gnome.org/pub/GNOME/binaries/win32/glade3/3.6/glade3-3.6.7-with-GTK+.exe)
+    * GTK+ 2.16.2.1602
+    * cairo 1.8.6
+
+# イメージ #
+
+![HelloCairo](https://cdn-ak.f.st-hatena.com/images/fotolife/y/ytyaru/20160829/20160829194327.png)
+
+# ライセンス #
+
+このソフトウェアはCC0ライセンスです。
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png "CC0")](http://creativecommons.org/publicdomain/zero/1.0/deed.ja)
